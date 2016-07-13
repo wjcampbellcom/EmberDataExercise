bb9911ae2d0f81cbc58f40f2d27210d721e31a92
Bill Campbell <bill@fundysoftware.com>
2016-07-13 09:50:46 -0700

    initial commit

---
+++ api/src/main/java/com/fundysoftware/PricingApplication.java
@@ -0,0 +1,12 @@
+package com.fundysoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingApplication.class, args);
+	}
+}
